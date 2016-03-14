ce031e11f474c40cd58138858e245a2e3ede4a57
Ivo Neskovic <ivo.neskovic@gmail.com>
2016-03-14 21:53:25 +0100

    Initial commit

---
+++ app/src/main/java/gr/academic/city/sdmd/notifications/MainActivity.java
@@ -0,0 +1,57 @@
+package gr.academic.city.sdmd.notifications;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PROGRESS_NOTIFICATION_ID = 187;
+
+    private int notificationCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        findViewById(R.id.btn_create_notification).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+    }
+
+    private void sendNotification() {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        // This is bad (just for example purposes), get your string from resources
+        String text = "You have " + ++notificationCount + " annoying notifications.";
+        String notificationTitle = "Annoy-O-Tron";
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.stat_notify_error)  // the status icon
+                .setTicker(text)                                     // the status text
+                .setWhen(System.currentTimeMillis())                 // the time stamp
+                .setContentTitle(notificationTitle)                  // the label of the entry
+                .setContentText(text)                                // the contents of the entry
+                .setContentIntent(contentIntent)                     // The intent to send when clicked
+                .setAutoCancel(true)
+                .build();
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // PROGRESS_NOTIFICATION_ID allows you to update the notification later on.
+        mNotificationManager.notify(PROGRESS_NOTIFICATION_ID, notification);
+    }
+}
