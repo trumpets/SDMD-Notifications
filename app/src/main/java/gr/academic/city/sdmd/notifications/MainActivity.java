9772d76f0d9d66ca95904d106084813249f9c084
Ivo Neskovic <ivo.neskovic@gmail.com>
2020-03-01 16:29:15 +0100

    Updated gradle and migrated to androidx.

--- app/src/main/java/gr/academic/city/sdmd/notifications/MainActivity.java
+++ app/src/main/java/gr/academic/city/sdmd/notifications/MainActivity.java
@@ -6,9 +6,10 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
 
